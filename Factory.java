b7d1ca69a1718d2beceb06d4859d70404ab48d14
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2019-03-14 15:48:08 -0700

    Update Factory.java

--- Factory.java
+++ Factory.java
@@ -8,7 +8,7 @@ import java.io.*;
 import java.util.*;
 
 public class Factory {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String filename = "infile.txt";
         int nResources = args.length;
         int[] resources = new int[nResources];
@@ -18,6 +18,13 @@ public class Factory {
         int[] maxDemand = new int[nResources];
         int[] allocated = new int[nResources];
         Thread[] workers = new Thread[Customer.COUNT];      // the customers
+        char choice = 'a';
+        String ch;
+        boolean loop = true;
+        boolean havent_ran_this = true;
+
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 
         try{
 		// read in values and initialize the matrices
@@ -45,15 +52,29 @@ public class Factory {
                 //++threadNum;        //theBank.getCustomer(threadNum);
                 //resourceNum = 0;
         }
-            
+
         } catch (FileNotFoundException fnfe) { throw new Error("Unable to find file \"" + filename + "\"");
         } catch (IOException ioe) { throw new Error("Error processing \"" + filename + "\""); }
+        do{
+          havent_ran_this = false;
+          System.out.println("Would you like to run Interactive mode(i), or Automatic Mode(a)?");
+          choice =  scanner.nextLine().charAt(0);
+          System.out.println("char is " + (char)choice);
 
-        System.out.println("FACTORY: created threads");     // start the customers
+        if(choice == 'a'){
+          havent_ran_this = false;
+          System.out.println("FACTORY: created threads");     // start the customers
 
-        System.out.println("FACTORY: started threads");
-        for (int i = 0; i < Customer.COUNT; i++) { workers[i].start(); }
-        
+          System.out.println("FACTORY: started threads");
+          for (int i = 0; i < Customer.COUNT; i++) { workers[i].start(); }
+      } else if(choice == 'i'){
 
+          System.out.print("Enter * to get the system state, OR enter <RQ | RL> <customer number> <resource #0> <#1> <#2>");
+          choice = scanner.nextLine().charAt(0);
+          if(choice == '*'){
+            theBank.getState();
+          }
+        }
+      }while(!theBank.isDone());
     }
 }
